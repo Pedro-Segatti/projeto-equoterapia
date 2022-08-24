8b7d908072c5154cf00162750fbe25c27d1e1989
Eduardo Vitor Mokfa <edumokfa@hotmail.com>
2022-08-23 22:12:47 -0300

    Menu alterado

--- patronato/src/view/menu.js
+++ patronato/src/view/menu.js
@@ -1,93 +1,41 @@
-import React, { useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import $ from 'jquery';
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Navbar = () => {
-  function animation() {
-    var tabsNewAnim = $('#navbarSupportedContent');
-    var activeItemNewAnim = tabsNewAnim.find('.active');
-    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-    var itemPosNewAnimTop = activeItemNewAnim.position();
-    var itemPosNewAnimLeft = activeItemNewAnim.position();
-    $(".hori-selector").css({
-      "top": itemPosNewAnimTop.top + "px",
-      "left": itemPosNewAnimLeft.left + "px",
-      "height": activeWidthNewAnimHeight + "px",
-      "width": activeWidthNewAnimWidth + "px"
-    });
-    $("#navbarSupportedContent").on("click", "li", function (e) {
-      $('#navbarSupportedContent ul li').removeClass("active");
-      $(this).addClass('active');
-      var activeWidthNewAnimHeight = $(this).innerHeight();
-      var activeWidthNewAnimWidth = $(this).innerWidth();
-      var itemPosNewAnimTop = $(this).position();
-      var itemPosNewAnimLeft = $(this).position();
-      $(".hori-selector").css({
-        "top": itemPosNewAnimTop.top + "px",
-        "left": itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px"
-      });
-    });
-  }
-
-  useEffect(() => {
-    animation();
-    $(window).on('resize', function () {
-      setTimeout(function () { animation(); }, 500);
-    });
-
-  }, []);
-
+const Menu = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-mainbg">
+    <Navbar sticky='top' expand="lg">
+      <Navbar.Brand className="mx-3" href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle className="bg-ligth" aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <NavDropdown title="Cadastros" id="basic-nav-dropdown" className="mr-4">
+            <NavDropdown.Item href="#action/3.1">Pacientes</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Funcionarios</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">....</NavDropdown.Item>
+          </NavDropdown>
 
-      <NavLink className="navbar-brand navbar-logo" to="/novaPagina" exact>
-        Patronato
-      </NavLink>
+          <NavDropdown title="Movimentos" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Pacientes</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Funcionarios</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">....</NavDropdown.Item>
+          </NavDropdown>
 
-      <button
-        className="navbar-toggler"
-        onClick={function () {
-          setTimeout(function () { animation(); });
-        }}
-        type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <i className="fas fa-bars text-white"></i>
-      </button>
+          <NavDropdown title="Consultas" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Pacientes</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Funcionarios</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">....</NavDropdown.Item>
+          </NavDropdown>
 
-      <div
-        className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-
-          <div className="hori-selector">
-            <div className="left"></div>
-            <div className="right"></div>
-          </div>
-
-          <li className="nav-item active">
-            <NavLink className="nav-link" to="/novaPagina" exact>
-              <i className="far fa-calendar-alt"></i>
-              Agenda
-            </NavLink>
-          </li>
-
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/outraPagina" exact>
-              <i className="far fa-address-book"></i>
-              Cadastros
-            </NavLink>
-          </li>
-
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/service" exact>
-              <i className="far fa-copy"></i>
-              Relatórios
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          <NavDropdown title="Relatórios" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Pacientes</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Funcionarios</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">....</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
-export default Navbar;
+export default Menu;
